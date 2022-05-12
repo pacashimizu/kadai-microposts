9bdea3624b0c411ef5b40d23f939b9a1fb89be2c
pac.a.shimizu <pac.a.shimizu@gmail.com>
2022-05-12 06:50:53 +0000

    favorite

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create(name: "taro", email: "taro@techacademy.jp", password: "taro1234", password_confirmation: "taro1234")
+User.create(name: "hanako", email: "hanako@techacademy.jp", password: "hanako1234", password_confirmation: "hanako1234")
+User.create(name: "kenji", email: "kenji@techacademy.jp", password: "kenji1234", password_confirmation: "kenji1234")
+
+5.times do |i|
+  Micropost.create(content: "message#{i+11}", user_id: 1)
+end
+
+5.times do |i|
+  Micropost.create(content: "message#{i+21}", user_id: 2)
+end
+
+5.times do |i|
+  Micropost.create(content: "message#{i+31}", user_id: 3)
+end
